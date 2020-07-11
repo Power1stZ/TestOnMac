f8a836cce449213d691b0b9f1fca1e60e878ad46
Nattapong Kueakool <nattpongfirst42@gmail.com>
2020-07-12 02:08:27 +0700

    Updates

---
+++ precache-manifest.090a767eea42e31bdbc7f410c48b5f43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dee878c1c69a19bd90085704f639b54",
+    "url": "/https//Power1stZ.github.io/TestOnMac/index.html"
+  },
+  {
+    "revision": "6f19c0596747c88ee1e6",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/css/main.b7cbb21f.chunk.css"
+  },
+  {
+    "revision": "55ac9390926fd8cbd67a",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/js/2.28c7c82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/js/2.28c7c82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f19c0596747c88ee1e6",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/js/main.a0632eb6.chunk.js"
+  },
+  {
+    "revision": "0ab144962fe6038a7cba",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/js/runtime-main.049c1ee5.js"
+  },
+  {
+    "revision": "1ecdbcf271dfa34106c1676bb3cfefae",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/media/null.1ecdbcf2.png"
+  },
+  {
+    "revision": "6eff53a94a8543c33e6589a3103816ed",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/media/paper.6eff53a9.png"
+  },
+  {
+    "revision": "57550028d3318edaf83f617d331e669f",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/media/rock.57550028.png"
+  },
+  {
+    "revision": "ce680009d92b87eaad945beafca5ec76",
+    "url": "/https//Power1stZ.github.io/TestOnMac/static/media/scissors.ce680009.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/https//Power1stZ.github.io/TestOnMac/precache-manifest.090a767eea42e31bdbc7f410c48b5f43.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//Power1stZ.github.io/TestOnMac/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
