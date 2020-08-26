996bca7c6e70e2edf8446151440575071be83016
Nattapong Kueakool <nattpongfirst42@gmail.com>
2020-08-26 13:51:13 +0700

    what

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router,Route } from 'react-router-dom';
 import "./App.css";
-import { Game } from "./pages/Game/Game.js";
-import { Menu } from "./pages/Menu/Menu.js";
-import { Home } from "./pages/Home/Home.js";
 
 function App() {
 
   return (
     <div>
-      <Router>
-        <Route path="/home" component={Home} />
-        <Route path="/game" component={Game} />
-        <Route path="/menu" component={Menu} />
-      </Router>
+      Hello
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Route,Link,BrowserRouter as Router,Switch} from 'react-router-dom';
+import { Game } from "./pages/Game/Game";
+import { Menu } from "./pages/Menu/Menu";
+import { Home } from "./pages/Home/Home";
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ App } />
+        <Route path="/menu" component={ Menu } />
+        <Route path="/game" component={ Game } />
+        <Route path="/home" component={ Home } />
+      </Switch>
+    </div>
+  </Router>
+)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(routing ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
